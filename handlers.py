fb86522ff9141f6acb09bbdb26bd8feaebcf1edf
slon_hk <st3panov.len@yandex.ru>
2024-06-21 23:24:50 +0300

    update

--- handlers.py
+++ handlers.py
@@ -22,43 +22,50 @@ class FSMFillForm(StatesGroup):
     fill_houre = State()
 
 #Start message and registration
-
 @router.message(CommandStart())
 async def cmd_start(message: types.Message, state: FSMContext):
-    await message.answer("Привет это бот для подсчета часов и зарплаты", reply_markup=kb.keyboard_main)
+    await message.answer("""Привет это бот для подсчета часов и зарплаты!\n
+                            Для начала внесите свою ставку, нажав на кнопку 'ставка'\n
+                            А потом можете вносить отработанные часы с помощбю кнопки 'Внести часы'\n
+                            Чтобы посмотреть все отработанные часы за месяц нажмите на кнопку 'Количество часов\n\n
+                            Приятного пользования!'""", reply_markup=kb.keyboard_main)
     db.start()
     
+# Обработчик для команды "Ставка"
 @router.message(F.text == "Ставка")
-async def fill_rate(message: types.Message, state: FSMContext):
+async def fill_rate_start(message: types.Message, state: FSMContext):
     await message.answer(text='Введите свою ставку')
     await state.set_state(FSMFillForm.fill_rate)
+
+# Обработчик для состояния fill_rate
 @router.message(StateFilter(FSMFillForm.fill_rate))
 async def fill_rate(message: types.Message, state: FSMContext):
-    rate = message.text()
-    if rate.content_type == 'text':
-        try:
-            rate = int(rate)
-            db.fill_rate(tg_id=message.from_user.id, rate=rate)
-            await message.answer(text="✅")
-            await state.clear()
-        except TypeError or ValueError:
-            await message.answer(text="Это не являеться числом, введите число")
-            
-
-
-@router.message(F.text == "Внести часы")
-async def send_voice_message(message: types.Message, state: FSMContext):
-    await message.answer(text = "Внесите количество часов которые вы отработали сегодня")
-    await state.set_state(FSMFillForm.fill_houre)
-
-@router.message(StateFilter(FSMFillForm.fill_houre))
-async def get_houre(message: types.Message, state: FSMContext):
-    try:
-        db.fill_houre(tg_id=message.from_user.id, houre=float(message.text))
+    rate = message.text
+    if rate.isdigit():
+        rate = int(rate)
+        db.fill_rate(tg_id=message.from_user.id, rate=rate)
         await message.answer(text="✅")
         await state.clear()
-    except TypeError or ValueError:
-            await message.answer(text="Это не являеться числом, введите число")
+    else:
+        await message.answer(text="Это не является числом, введите число")
+
+# Обработчик для команды "Внести часы"
+@router.message(F.text == "Внести часы")
+async def send_voice_message(message: types.Message, state: FSMContext):
+    await message.answer(text="Внесите количество часов, которые вы отработали сегодня")
+    await state.set_state(FSMFillForm.fill_houre)
+
+# Обработчик для состояния fill_houre
+@router.message(StateFilter(FSMFillForm.fill_houre))
+async def get_houre(message: types.Message, state: FSMContext):
+    houre = message.text
+    try:
+        houre = float(houre)
+        db.fill_houre(tg_id=message.from_user.id, houre=houre)
+        await message.answer(text="✅")
+        await state.clear()
+    except ValueError:
+        await message.answer(text="Это не является числом, введите число")
 
 @router.message(F.text == "Количество часов")
 async def get_houre(message: types.Message):
